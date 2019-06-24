f761d2bda79c1ce7bebe2018f767317bd8d71f8d
sh7ning <sh7ning@qq.com>
2019-06-24 21:50:29 +0800

    d

--- helper/helper.go
+++ helper/helper.go
@@ -2,26 +2,6 @@ package helper
 
 import "encoding/json"
 
-//func DecimalDiff(a string, b string) (bool, error) {
-//	if a == b {
-//		return true, nil
-//	}
-//
-//	aF, err := decimal.NewFromString(a)
-//	if err != nil {
-//		return false, err
-//	}
-//	bF, err := decimal.NewFromString(b)
-//	if err != nil {
-//		return false, err
-//	}
-//
-//	if !aF.Equal(bF) {
-//		return false, nil
-//	}
-//
-//	return true, nil
-//}
 func Min(x, y int) int {
 	if x < y {
 		return x
