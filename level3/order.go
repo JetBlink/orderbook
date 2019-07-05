dadfef07a90cca4e9e11c9cb9baf11b8119a9428
sh7ning <sh7ning@qq.com>
2019-07-05 15:51:08 +0800

    add order with info

--- level3/order.go
+++ level3/order.go
@@ -11,6 +11,7 @@ type Order struct {
 	Price   decimal.Decimal
 	Size    decimal.Decimal
 	Time    uint64
+	Info    interface{}
 }
 
 func NewOrder(orderId string, price string, size string, time uint64) (order *Order, err error) {
@@ -29,6 +30,27 @@ func NewOrder(orderId string, price string, size string, time uint64) (order *Or
 		Price:   priceValue,
 		Size:    sizeValue,
 		Time:    time,
+	}
+	return
+}
+
+func NewOrderWithInfo(orderId string, price string, size string, time uint64, info interface{}) (order *Order, err error) {
+	priceValue, err := decimal.NewFromString(price)
+	if err != nil {
+		return nil, fmt.Errorf("NewOrder failed, price: `%s`, error: %v", price, err)
+	}
+
+	sizeValue, err := decimal.NewFromString(size)
+	if err != nil {
+		return nil, fmt.Errorf("NewOrder failed, size: `%s`, error: %v", size, err)
+	}
+
+	order = &Order{
+		OrderId: orderId,
+		Price:   priceValue,
+		Size:    sizeValue,
+		Time:    time,
+		Info:    info,
 	}
 	return
 }
